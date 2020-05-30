030ac563ddafe3855e5fa63297b44b99f88e02ac
Naomi Ware <nware1066@yahoo.com>
2020-05-30 15:34:44 -0600

    Created selectStudy function

--- main.js
+++ main.js
@@ -5,3 +5,36 @@ var startActivityButton = document.querySelector('.start-activity-button');
 var studyImg = document.querySelector('.study-img');
 var studyActiveImg = document.querySelector('.study-active-img');
 var studyButtonActive = document.querySelector('.study-button-active');
+
+studyButton.addEventListener('click', selectStudy);
+
+
+
+function selectStudy(event) {
+  event.preventDefault();
+  studyImg.src = "./assets/study-active.svg";
+}
+
+
+
+
+// Form Functionality
+// When an activity category is clicked on (Exercise, Meditate, or Study),
+// the associated border and icon should change colors to give a visual indication that it has been selected.
+// Colors are provided in comp.
+//
+// An input field should be provided for What would you like to accomplish during this time?,
+// minutes and seconds. The minutes and seconds fields should only accept numbers.
+// (Hint: more than one layer should probably be put into place to ensure this.
+//   Make sure that e cannot be accepted.)
+//
+// A Start Activity button is provided to submit the data entered into the form.
+// When the button is clicked, update your data model with an instance of the Activity class.
+//
+// When the Start Activity button is clicked, the user should no longer see the form,
+// and instead see a timer clock. The timer clock should display the user-provided minutes and seconds,
+// as well as the description. The category should not appear, but the outline of the circle
+// should match the color associated with the category.
+//
+// If the Start Activity button is clicked before the user has entered information into all four inputs,
+// the user will receive an error message, but will not lose any information that was provided.
